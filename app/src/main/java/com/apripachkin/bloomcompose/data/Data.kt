8c110da308b6843be6da850136a77d4a5cc5fb7c
Anton Pripachkin <apripachkin@lohika.com>
2021-06-16 14:59:19 +0300

    Added sample data

---
+++ app/src/main/java/com/apripachkin/bloomcompose/data/Data.kt
@@ -0,0 +1,16 @@
+package com.apripachkin.bloomcompose.data
+
+data class Data(val name: String, val url: String)
+val sampleData = listOf(
+  Data("Desert chic", "https://www.pexels.com/photo/assorted-color-flowers-2132227/"),
+  Data("Tiny terrariums", "https://www.pexels.com/photo/clear-glass-terrarium-jar-with-mossy-plants-1400375/"),
+  Data("Jungle vibes", "https://www.pexels.com/photo/big-green-leaves-of-monstera-5699665/"),
+  Data("Easy care", "https://www.pexels.com/photo/green-plant-with-long-leaves-in-pot-at-home-6208086/"),
+  Data("Statements", "https://www.pexels.com/photo/green-leaf-plant-indoors-3511755/"),
+  Data("Monstera", "https://www.pexels.com/photo/green-swiss-cheese-plant-3097770/"),
+  Data("Aglaonema", "https://www.pexels.com/photo/green-leaf-plant-on-white-stones-4751978/"),
+  Data("Peace lily", "https://www.pexels.com/photo/delicate-spathiphyllum-cochlearispathum-flowers-with-fresh-green-leaves-in-garden-4425201/"),
+  Data("Fiddle leaf", "https://www.pexels.com/photo/ficus-lyrata-with-lush-green-leaves-in-house-6208087/"),
+  Data("Snake plant", "https://www.pexels.com/photo/photo-of-green-snake-house-plant-2123482/"),
+  Data("Pothos", "https://www.pexels.com/photo/green-leaf-plant-1084199/")
+)
