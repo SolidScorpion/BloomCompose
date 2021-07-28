44ac00df9569cb4622a457945da560e9a1076e48
Anton Pripachkin <apripachkin@lohika.com>
2021-07-28 14:48:51 +0300

    Small changes

--- app/src/main/java/com/apripachkin/bloomcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/apripachkin/bloomcompose/ui/theme/Theme.kt
@@ -40,7 +40,7 @@ private val LightColorPalette = lightColors(
 @Composable
 fun BloomComposeTheme(
   darkTheme: Boolean = isSystemInDarkTheme(),
-  content: @Composable() () -> Unit
+  content: @Composable () -> Unit
 ) {
   val colors = if (darkTheme) {
     DarkColorPalette
