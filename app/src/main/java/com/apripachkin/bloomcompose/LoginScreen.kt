5b1686e293b8553e772129798175f4a42cf4b9a0
Anton P <solidscorpion@gmail.com>
2021-08-02 16:16:53 +0300

    Accepting text input

--- app/src/main/java/com/apripachkin/bloomcompose/LoginScreen.kt
+++ app/src/main/java/com/apripachkin/bloomcompose/LoginScreen.kt
@@ -2,17 +2,25 @@ package com.apripachkin.bloomcompose
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,7 +29,8 @@ import com.apripachkin.bloomcompose.ui.theme.BloomComposeTheme
 @Composable
 fun LoginScreen() {
   Surface(
-    color = MaterialTheme.colors.background
+    color = MaterialTheme.colors.background,
+    modifier = Modifier.fillMaxSize()
   ) {
     Column(
       horizontalAlignment = Alignment.CenterHorizontally,
@@ -52,9 +61,15 @@ private fun LoginButton() {
 
 @Composable
 private fun PasswordInput() {
+  var text by remember {
+    mutableStateOf("")
+  }
   OutlinedTextField(
-    value = "",
-    onValueChange = {},
+    value = text,
+    onValueChange = {
+                    text = it
+    },
+    visualTransformation = PasswordVisualTransformation() ,
     label = {
       Text(text = "Password (8+ characters)")
     },
@@ -64,13 +79,21 @@ private fun PasswordInput() {
 
 @Composable
 private fun EmailInput() {
+  var textState by remember {
+    mutableStateOf("")
+  }
   OutlinedTextField(
-    value = "",
-    onValueChange = {},
+    value = textState,
+    onValueChange = {
+      textState = it
+    },
     label = {
       Text(text = "Email address")
     },
-    modifier = Modifier.fillMaxWidth()
+    keyboardOptions = KeyboardOptions.Default.copy(
+      keyboardType = KeyboardType.Email
+    ),
+    modifier = Modifier.fillMaxWidth(),
   )
 }
 
